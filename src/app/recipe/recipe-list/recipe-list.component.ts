fdbd7f882cc6c059b0288feb67fa3f57ff52e227
tisha512 <tahmina_tisha@naiyani.com>
2023-08-06 10:03:14 +0600

    recipe model create, and add content and display

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Fruit custard',
+      'Made with fresh fruits, milk',
+      'https://www.vegrecipesofindia.com/wp-content/uploads/2021/11/fruit-custard-4.jpg'
+    ),
+    new Recipe(
+      'Fruit Salad',
+      'Made with seasonal fruits',
+      'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
+    ),
+  ];
+  constructor() {}
 }
---
+++ src/app/recipe/recipe.model.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+  // blueprint of every recipe item : how does it look like
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
