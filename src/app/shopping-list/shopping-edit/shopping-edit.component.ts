864a13e2aa6c082b4154aa7c4bc6990ea1a04d65
tisha512 <tahmina_tisha@naiyani.com>
2023-08-06 14:42:18 +0600

    dynamic data input and display using property and event binding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -10,7 +10,6 @@ export class ShoppingEditComponent {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
   onAddItem() {
     const ingName=this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
