e1f8e8aafa717c4b7d7d1c585069944fdb36f0b7
tisha512 <tahmina_tisha@naiyani.com>
2023-08-09 10:44:21 +0600

    Forms: add, edit, delete, clear button execution

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,12 +5,16 @@ import { Subject } from 'rxjs';
 export class ShoppingListService {
   //inform other component that a data is available
   ingredientChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 10),
     new Ingredient('Orange', 5),
   ];
   getIngredients() {
     return this.ingredients.slice();
+  }
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
@@ -26,6 +30,14 @@ export class ShoppingListService {
   }
   addIngredients(ingredient: Ingredient[]) {
     this.ingredients.push(...ingredient);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientChanged.next(this.ingredients.slice());
   }
 }
